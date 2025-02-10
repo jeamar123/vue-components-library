d1bc4a9dbf3fcf28d25f3551579512466428b8b6
Jeamar Libres <jeamarlibres@Jeamars-Mac-mini.local>
2025-02-10 22:10:48 +0800

    updated Button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,8 +1,8 @@
 
 
 <template>
-  <button :class="getClasses">
-    <slot>Button</slot>
+  <button :class="[getClasses]" :disabled="disabled">
+    <slot />
   </button>
 </template>
 
@@ -11,10 +11,12 @@ import { computed } from 'vue'
 
 interface Props {
   variant?: string;
+  disabled?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   variant: 'primary' , // primary, secondary
+  disabled: false
 })
 
 const getClasses = computed(() => {
@@ -48,5 +50,35 @@ const getClasses = computed(() => {
 </script>
 
 <style scoped>
+.btn {
+  white-space: nowrap;
+  border-radius: 0.375rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+  padding-top: 0.75rem;
+  padding-bottom: 0.75rem;
+  font-weight: 600;
+  -webkit-font-smoothing: antialiased;
+  border: 1px solid transparent;
+}
 
+.btn:disabled {
+  @apply opacity-50 pointer-events-none;
+}
+
+.btn-primary {
+  @apply bg-primary text-white hover:bg-[#6366f1];
+}
+
+.btn-secondary {
+  @apply bg-secondary text-[#4f46e5] hover:bg-[#e0e7ff];
+}
+
+.btn-outline {
+  @apply bg-white text-primary border-primary hover:opacity-80;
+}
+
+.btn-default {
+  @apply bg-white text-[#4b5563] border-[#d1d5db] hover:bg-[#f9fafb];
+}
 </style>
